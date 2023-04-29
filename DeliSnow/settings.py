882d5fd4af8cad1f4b6a1aea134626fc3d662f75
Tachoramirez <eustacioramirez82@gmail.com>
2023-04-28 22:47:15 -0600

    Comenté la secret key en settings

--- DeliSnow/settings.py
+++ DeliSnow/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uu8pdpd^$rdk*%)%^-9-lxr3+h@%%1pg^mul8$qq@wj8+h_o*y'
+#SECRET_KEY = 'django-insecure-uu8pdpd^$rdk*%)%^-9-lxr3+h@%%1pg^mul8$qq@wj8+h_o*y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
