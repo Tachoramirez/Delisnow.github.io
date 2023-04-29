cde47c315be4365ae80ace9952a3d8e6a92758d2
Tachoramirez <eustacioramirez82@gmail.com>
2023-04-28 23:34:34 -0600

    declarar la index url

--- DeliSnow/urls.py
+++ DeliSnow/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Home.urls')),
+    path('/', include('Home.urls')),
     path('autenticacion/', include('Autenticacion.urls')),
     path('clientes/', include('Clientes.urls')),
 
